1fae4e4f93b608ab7a06cda41b41c52d532e700a
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-02-21 13:56:54 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>laravel-migration-seeder</title>
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    <h1>laravel-migration-seeder</h1>
+</body>
+
+</html>
