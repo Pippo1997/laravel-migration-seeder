d1c73a560401fe2f471c485a0886817e7a648253
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-02-21 14:17:40 +0100

    creazione tabella trains migration

---
+++ database/migrations/2023_02_21_130742_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 100);
+            $table->string('Stazione_di_partenza', 50);
+            $table->string('Stazione_di_arrivo', 50);
+            $table->string('Orario_di_partenza', 30);
+            $table->string('Orario_di_arrivo', 30);
+            $table->smallInteger('Codice_Treno', 4);
+            $table->smallInteger('Numero_Carrozze', 25);
+            $table->time('In_orario');
+            $table->boolean('Cancellato');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
